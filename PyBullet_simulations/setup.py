a1ea2a53a3ca8dc0504556561177f1d486f81fbb
Alessandra Tafuro <taffi98.at@gmail.com>
2021-12-11 16:22:17 +0000

    Add files via upload

---
+++ PyBullet_simulations/setup.py
@@ -0,0 +1,8 @@
+#Setup code for panda_gym environment#
+from setuptools import setup
+
+setup(
+    name="panda_gym",
+    version='0.0.1',
+    install_requires=['gym', 'pybullet', 'numpy', 'matplotlib']
+)
